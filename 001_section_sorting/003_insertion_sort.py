28745615df17f8caecfeb3068b1bd334411ad815
Konstantin_Victorovich <kbrb1515@gmail.com>
2023-07-08 01:29:34 +0300

    README.md modified

--- 001_section_sorting/003_insertion_sort.py
+++ 001_section_sorting/003_insertion_sort.py
@@ -40,14 +40,14 @@ def insertion_sort(arr):
     n = len(arr)
 
     for i in range(1, n):
-        key = arr[i]
         j = i - 1
 
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-
-        arr[j + 1] = key
+        while j >= 0:
+            if arr[j] > arr[j + 1]:
+                arr[j + 1], arr[j] = arr[j], arr[j + 1]
+                j -= 1
+            else:
+                break
 
     return arr
 
